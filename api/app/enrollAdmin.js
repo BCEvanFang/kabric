279c2bce62212bc2efb65c0979fa2e9676ad3cd0
TW-MONEYSQ\evanfang <evanfang@moneysq.com>
2018-11-15 10:12:00 +0800

    重構 api/app/enrollAdmin.js: 抽出getClient, getCAClient方法

--- api/app/enrollAdmin.js
+++ api/app/enrollAdmin.js
@@ -14,51 +14,32 @@ var Fabric_CA_Client = require("fabric-ca-client");
 var path = require("path");
 
 async function enrollAdmin() {
-  //
-  var fabric_client = new Fabric_Client();
-  var fabric_ca_client = null;
-  var admin_user = null;
+
   var store_path = path.join(__dirname, "hfc-key-store");
   console.log(" Store path:" + store_path);
 
-  // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
-  let state_store = await Fabric_Client.newDefaultKeyValueStore({
-    path: store_path
-  });
+  // Get fabric client
+  var fabric_client = await getClient(store_path);
 
-  fabric_client.setStateStore(state_store);
-  var crypto_suite = Fabric_Client.newCryptoSuite();
-  // use the same location for the state store (where the users' certificate are kept)
-  // and the crypto store (where the users' keys are kept)
-  var crypto_store = Fabric_Client.newCryptoKeyStore({ path: store_path });
-  crypto_suite.setCryptoKeyStore(crypto_store);
-  fabric_client.setCryptoSuite(crypto_suite);
-  var tlsOptions = {
-    trustedRoots: [],
-    verify: false
-  };
-  // be sure to change the http to https when the CA is running TLS enabled
-  fabric_ca_client = new Fabric_CA_Client(
-    "http://localhost:7054",
-    tlsOptions,
-    "ca.example.com",
-    crypto_suite
-  );
+  // Get admin user
+  let admin_user = await fabric_client.getUserContext("admin", true);
 
-  ///
-  let user_from_store = await fabric_client.getUserContext("admin", true);
-
-  if (user_from_store && user_from_store.isEnrolled()) {
+  // user already exists
+  if (admin_user && admin_user.isEnrolled()) {
     console.log("Successfully loaded admin from persistence");
-    admin_user = user_from_store;
     return null;
   }
 
+  // be sure to change the http to https when the CA is running TLS enabled
+  let fabric_ca_client = getCAClient(store_path);
+
+  // Enroll admin user
   let enrollment = await fabric_ca_client.enroll({
     enrollmentID: "admin",
     enrollmentSecret: "adminpw"
   });
 
+  // Set admin user to fabric client context
   let user = await fabric_client.createUser({
     username: "admin",
     mspid: "Org1MSP",
@@ -74,6 +55,49 @@ async function enrollAdmin() {
 
   console.log(
     "Assigned the admin user to the fabric client ::" + admin_user.toString()
+  );
+}
+
+async function getClient(store_path) {
+  var fabric_client = new Fabric_Client();
+
+  // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
+  let state_store = await Fabric_Client.newDefaultKeyValueStore({
+    path: store_path
+  });
+
+  fabric_client.setStateStore(state_store);
+  var crypto_suite = Fabric_Client.newCryptoSuite();
+  
+  // use the same location for the state store (where the users' certificate are kept)
+  // and the crypto store (where the users' keys are kept)
+  var crypto_store = Fabric_Client.newCryptoKeyStore({ path: store_path });
+  crypto_suite.setCryptoKeyStore(crypto_store);
+  fabric_client.setCryptoSuite(crypto_suite);
+
+  return fabric_client;
+}
+
+/// Get fabric CA Client
+function getCAClient(store_path) {
+  let crypto_suite = Fabric_Client.newCryptoSuite();
+
+  // use the same location for the state store (where the users' certificate are kept)
+  // and the crypto store (where the users' keys are kept)
+  let crypto_store = Fabric_Client.newCryptoKeyStore({ path: store_path });
+  crypto_suite.setCryptoKeyStore(crypto_store);
+
+  let tlsOptions = {
+    trustedRoots: [],
+    verify: false
+  };
+
+  // be sure to change the http to https when the CA is running TLS enabled
+  return new Fabric_CA_Client(
+    "http://localhost:7054",
+    tlsOptions,
+    "ca.example.com",
+    crypto_suite
   );
 }
 
