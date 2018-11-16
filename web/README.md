93c4a62f5cbc2cf06cc01de0037d501522b8e938
TW-MONEYSQ\evanfang <evanfang@moneysq.com>
2018-11-16 09:13:19 +0800

    增加HTML頁面, 可以透過API從chaincode取得資料

--- web/README.md
+++ web/README.md
@@ -4,3 +4,9 @@ Start web service
 ```
 node app.js
 ```
+
+Test web api
+```
+url -s -X GET "http://localhost:3001/kabric-chaincode"
+```
+>記得先啟動basic-network, api/app server
--- web/app.js
+++ web/app.js
@@ -1,13 +1,26 @@
-var express = require('express');
+var express = require("express");
+var request = require("request");
+
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get("/kabric-chain-code", async (req, res) => {
-    res.send("hello kabric from web api");
-})
+app.get("/kabric-chaincode", (req, res) => {
+  
+    request("http://localhost:4001/query", function(error, response, body) {
+    
+    console.log("error:", error);
+    console.log("statusCode:", response && response.statusCode);
+    console.log("body:", body);
+    
+    res.send({
+      error: error,
+      response: response,
+      body: body
+    });
+  });
+});
 
-
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!');
+app.listen(3001, function() {
+  console.log("Example app listening on port 3001!");
 });
